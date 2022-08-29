7b310775dd092ead300ca75d56b350b6fc979b00
Demetrius L Ricarte <dlricarte@gmail.com>
2022-08-28 21:49:37 -0300

    Projeto 2 de infraestrutura

---
+++ dio/linux_experience/iac2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Atualizar Servidor
+apt-get update -y
+apt-get upgrade -y
+#Instalar Apache
+apt-get install apache2 -y
+#Instalar Unzip
+apt-get install unzip -y
+#Apagar arquivo html do apache2
+rm /var/www/html/index.html
+#Acessar a pasta temporaria do linux
+cd /tmp
+#Baixar repositório do github do desafio
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+#Descompactar repositorio
+unzip main.zip
+#Mover arquivos do repositorio para pasta do Apache2
+mv /tmp/main/* /var/www/html
+#Apagar repositorio da pasta temporaria do linux
+rm -Rf /tmp/main
+#Apagar arquivo compactado do repositorio da pasta temporaria do linux
+rm main.zip
+# Dar permissão para usuário do apache para os arquivos copiados para pasta do apache
+chown www-data. -Rf /var/www/html
